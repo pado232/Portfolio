2905e06a11512f35d2dcd008e22bcbcbbd6e44f5
ehsilver98@gmail.com <ehsilver98@gmail.com>
2024-07-09 00:20:54 +0900

    고정 메뉴 버튼 추가

--- src/App.js
+++ src/App.js
@@ -6,8 +6,29 @@ import Project from "./pages/ProjectPage/Project";
 import Contact from "./pages/ContactPage/Contact";
 import Home from "./pages/Home/Home";
 import MyFooter from "./components/MyFooter/MyFooter";
+import { useEffect, useState } from "react";
+import FixedMenuButton from "./components/utils/button/FixedMenuButton";
 function App() {
   const location = useLocation();
+  const [showButton, setShowButton] = useState(false);
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 200) {
+      // 예시로 100 픽셀 이상 스크롤되면 버튼을 보여줍니다.
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  // 페이지 로드 시 스크롤 이벤트 리스너 추가
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Menu />
@@ -17,6 +38,7 @@ function App() {
         <Route path="/project" element={<Project />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      {showButton && <FixedMenuButton />}
       {location.pathname !== "/" && <MyFooter />}
     </div>
   );
---
+++ src/components/utils/button/FixedMenuButton.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import styles from "./FixedMenuButton.module.css";
+import { useNavigate } from "react-router-dom";
+import { LuArrowBigUpDash } from "react-icons/lu";
+
+const FixedMenuButton = () => {
+  const navigate = useNavigate();
+  const [mouseOn, setMouseOn] = useState(false);
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  return (
+    <div
+      className={styles.FixedMenuButton}
+      onMouseLeave={() => setMouseOn(false)}
+      onMouseEnter={() => setMouseOn(true)}
+    >
+      <button className={styles.fix_btn}>+</button>
+      <div className={`${styles.btn_wrapper} ${mouseOn ? styles.visible : ""}`}>
+        <div>
+          <button
+            className={styles.goto}
+            onClick={() => {
+              navigate("/intro");
+            }}
+          >
+            INTRO
+          </button>
+        </div>
+        <div>
+          <button
+            className={styles.goto}
+            onClick={() => {
+              navigate("/project");
+            }}
+          >
+            PROJECT
+          </button>
+        </div>
+        <div>
+          <button
+            className={styles.goto}
+            onClick={() => {
+              navigate("/contact");
+            }}
+          >
+            CONTACT
+          </button>
+        </div>
+        <div>
+          <button
+            className={styles.goto}
+            onClick={() => {
+              scrollToTop();
+            }}
+          >
+            Top
+            <LuArrowBigUpDash size={23} style={{ verticalAlign: -6 }} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FixedMenuButton;
