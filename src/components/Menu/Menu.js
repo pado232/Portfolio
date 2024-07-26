26b120b8aecdc5005d9b2786d2751ad19aaf0c45
ehsilver98@gmail.com <ehsilver98@gmail.com>
2024-07-26 13:01:23 +0900

    메뉴 디자인 수정(홈 버튼 만들기 추가)

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,20 +1,98 @@
-import { useNavigate } from "react-router-dom";
-import styles from "./Menu.module.css";
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { VscKey } from "react-icons/vsc";
+import { TfiLock } from "react-icons/tfi";
+import lightStyles from "./Menu.module.css";
+import darkStyles from "./MenuD.module.css";
+import { useTheme } from "../../ThemeContext/ThemeContext";
+
 const Menu = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [theme] = useTheme();
+  const [mouseOn, setMouseOn] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    setMousePosition({
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    });
+  };
+
+  const styles = theme === "light" ? lightStyles : darkStyles;
+
+  const handleHome = () => navigate("/");
+  const handleIntro = () => navigate("/intro");
+  const handleProject = () => navigate("/project");
+  const handleContact = () => navigate("/contact");
+
   return (
     <div className={styles.Menu}>
-      <ul>
-        <li>
-          <button onClick={() => navigate("/intro")}>INTRODUCTION</button>
-        </li>
-        <li>
-          <button onClick={() => navigate("/project")}>PROJECT</button>
-        </li>
-        <li>
-          <button onClick={() => navigate("/contact")}>CONTACT</button>
-        </li>
-      </ul>
+      <div
+        className={styles.home}
+        onMouseEnter={() => setMouseOn(true)}
+        onMouseLeave={() => setMouseOn(false)}
+        onMouseMove={(event) => handleMouseMove(event)}
+        onClick={handleHome}
+      >
+        <div className={styles.home_icon}>
+          <TfiLock size={"2.5rem"} style={{ marginRight: "0.7rem " }} />
+        </div>
+        <div>
+          <h2 className={styles.h2}>Lock</h2>
+          <p className={styles.p}>포트폴리오를 잠궈 다시 암호를 풀어보세요!</p>
+        </div>
+      </div>
+      {mouseOn && (
+        <div
+          className={styles.mouse_key}
+          style={{
+            top: `${mousePosition.y}px`,
+            left: `${mousePosition.x}px`,
+            backgroundPosition: `-${mousePosition.x * 2}px -${
+              mousePosition.y * 2
+            }px`,
+          }}
+        >
+          <VscKey className={styles.key_icon} size={"2rem"} /> click!
+        </div>
+      )}
+      <div className={styles.menu}>
+        <ul>
+          <li>
+            <button
+              className={`${styles.btn} ${
+                location.pathname === "/intro" ? styles.active : ""
+              }`}
+              onClick={handleIntro}
+            >
+              INTRODUCTION
+            </button>
+          </li>
+          <li>
+            <button
+              className={`${styles.btn} ${
+                location.pathname === "/project" ? styles.active : ""
+              }`}
+              onClick={handleProject}
+            >
+              PROJECT
+            </button>
+          </li>
+          <li>
+            <button
+              className={`${styles.btn} ${
+                location.pathname === "/contact" ? styles.active : ""
+              }`}
+              onClick={handleContact}
+            >
+              CONTACT
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
